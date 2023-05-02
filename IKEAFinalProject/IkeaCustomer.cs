43ff2cb2142496fa5186d1f87cf18e8dac077101
deonopowelljr <deonojr13@gmail.com>
2023-05-02 14:51:25 -0500

    this is IkeaCustomer

--- IKEAFinalProject/IkeaCustomer.cs
+++ IKEAFinalProject/IkeaCustomer.cs
@@ -9,7 +9,11 @@ namespace IKEAFinalProject
     public class IkeaCustomer
     {
 
-
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Date { get; set; }
 
     }
 }
