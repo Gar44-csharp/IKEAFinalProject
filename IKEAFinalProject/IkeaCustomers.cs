0a1ee7307929901e164e7275d0a9ce3625537c34
deonopowelljr <deonojr13@gmail.com>
2023-05-02 14:50:23 -0500

    this is a test!

--- IKEAFinalProject/IkeaCustomers.cs
+++ IKEAFinalProject/IkeaCustomers.cs
@@ -3,16 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static IKEAFinalProject.IkeaCustomers;
 
 namespace IKEAFinalProject
 {
     public class IkeaCustomers
     {
-        
 
+        internal class Customers
+        {
+            public List<IkeaCustomer> customers { get; set; }
 
+            public Customers()
+            {
+                customers = new List<IkeaCustomer>();
+            }
 
+            public IkeaCustomer Authenticate(string username, string password)
+            {
+                var c = customers.Where(o => (o.Username == username) && (o.Password == password));
 
+                if (c.Count() > 0)
+                {
+                    return c.First();
+                }
+                else
+                {
+                    return null;
+                }
+            }
 
+        }
     }
+
+
+
+
 }
+
