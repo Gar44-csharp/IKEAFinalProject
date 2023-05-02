1e5408ca16836eb745d0cf3237fa08cdaebbf4fd
Gar44-csharp <garrett.lukoski@marquette.edu>
2023-05-02 14:48:25 -0500

    These are new changes that I made.

--- IKEAFinalProject/IkeaEmployee.cs
+++ IKEAFinalProject/IkeaEmployee.cs
@@ -8,5 +8,31 @@ namespace IKEAFinalProject
 {
     public class IkeaEmployee
     {
+        private static int autoEmployeeInstance;
+        //
+        public int IkeaEmployeeID { get; set; }
+        //
+        public string EmployeeUsername { get; set; }
+        //
+        public string EmployeePassword { get; set; }
+        //
+        public string FirstName { get; set; }
+        //
+        public string LastName { get; set; }
+        //
+
+        public IkeaEmployee()
+        {
+            autoEmployeeInstance++;
+            //
+            IkeaEmployeeID = autoEmployeeInstance;
+            //
+        }
+
+
+
+
+
+
     }
 }
